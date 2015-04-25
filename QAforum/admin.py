c53ae43707aa307a99cfd30a6b2b36a6ed4f6905
saloni <saloni@saloni-Lenovo-B560.(none)>
2015-04-25 16:01:09 +0530

    admin created

--- QAforum/admin.py
+++ QAforum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import *
+admin.autodiscover()
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(UserProfile)
